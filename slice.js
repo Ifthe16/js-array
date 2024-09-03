c2ab9d4bdf39e95d93094e150a791254aa854bcb
Ifthe16 <habiburrahmanif975@gmail.com>
2024-09-03 12:42:39 +0600

    added the slice file of javascript

---
+++ slice.js
@@ -0,0 +1,6 @@
+const pizza = [1, 2, 3, 4, 5, 6, 7];
+console.log(pizza.slice(2,4));
+
+const fruits = ['apple', 'mango', 'banana', 'grape', 'berry','jackfruit', 'papaya'];
+
+console.log(fruits.slice(1,5));
