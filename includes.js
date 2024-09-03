b5a0e6df57d3f09d92dc232a11ec515ccbbefac3
Ifthe16 <habiburrahmanif975@gmail.com>
2024-09-03 11:24:28 +0600

    added the  includes file of javascript

---
+++ includes.js
@@ -0,0 +1,20 @@
+const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam'];
+
+console.log(friends.includes('gelam'));
+console.log(friends.includes('khailam'));
+
+if(friends.includes('salam')){
+    console.log('party')
+}
+else{
+    console.log('no food. we are fasting')
+}
+
+// includes() is case sensitive
+
+if(friends.includes('Salam')){
+    console.log('party')
+}
+else{
+    console.log('no food. we are fasting')
+}
